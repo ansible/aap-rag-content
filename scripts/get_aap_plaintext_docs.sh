dc51261ff678bfca1ef7c267aa80143c96ade2f8
Tami Takamiya <ttakamiy@redhat.com>
2024-10-02 22:05:51 -0400

    Initial content

---
+++ scripts/get_aap_plaintext_docs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]
+then
+  echo "Usage: get_aap_plaintext_docs.sh aap_version"
+  exit 1
+fi
+
+AAP_VERSION=$1
+BASE_DIR=aap-product-docs-plaintext
+
+set -eou pipefail
+
+trap "rm -rf aap-docs" EXIT
+
+if [ ! -d ${BASE_DIR} ]; then
+    mkdir ${BASE_DIR}
+fi
+
+rm -rf ${BASE_DIR}/${AAP_VERSION}
+
+git clone --single-branch --branch ${AAP_VERSION} https://github.com/ansible/aap-docs.git
+
+python scripts/asciidoctor-text/convert-it-all-aap.py -i aap-docs \
+    -o ${BASE_DIR}/${AAP_VERSION} -a aap-docs/downstream/attributes/attributes.adoc
+
+rm -rf ${BASE_DIR}/${AAP_VERSION}/archive
